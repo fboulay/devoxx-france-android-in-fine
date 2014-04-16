67f46bd5e9dba737b6005c670e7eba61fbcdd722
Christian Nguyen Van Than <christian.nguyenvanthan@gmail.com>
2014-04-16 23:57:08 +0200

    restore old behaviour by adding twitter4j

--- src/com/infine/android/devoxx/service/TwitterService.java
+++ src/com/infine/android/devoxx/service/TwitterService.java
@@ -14,17 +14,27 @@
 package com.infine.android.devoxx.service;
 
 import android.app.IntentService;
+import android.content.ContentProviderOperation;
 import android.content.ContentResolver;
 import android.content.Intent;
+import android.content.OperationApplicationException;
 import android.content.SharedPreferences;
+import android.os.RemoteException;
 import android.preference.PreferenceManager;
 import android.util.Log;
 
-import com.infine.android.devoxx.io.json.JsonHandler.HandlerException;
-import com.infine.android.devoxx.io.json.JsonTwitterHandler;
 import com.infine.android.devoxx.io.json.RemoteJsonExecutor;
+import com.infine.android.devoxx.provider.ScheduleContract;
 
-import org.apache.http.client.HttpClient;
+import java.util.ArrayList;
+
+import twitter4j.Query;
+import twitter4j.QueryResult;
+import twitter4j.Status;
+import twitter4j.Twitter;
+import twitter4j.TwitterException;
+import twitter4j.TwitterFactory;
+import twitter4j.conf.ConfigurationBuilder;
 
 public class TwitterService extends IntentService {
 
@@ -34,24 +44,32 @@ public class TwitterService extends IntentService {
 
 	public static final String EXTRA_STATUS_RECEIVER = "com.infine.android.devoxx.twitter.extra.STATUS_RECEIVER";
 
-	private RemoteJsonExecutor mExecutor;
+    private static final String LAST_TWEET_ID = "com.infine.android.devoxx.twitter.extra.last.tweet.id";
 
-	private static final String TWITTER_SEARCH_API_URL = "https://api.twitter.com/1.1/search/tweets.json";
-	private static final String DEFAULT_QUERY = TWITTER_SEARCH_API_URL + "?q=devoxxfr";
+    private ContentResolver mResolver;
 
-	public TwitterService() {
+    private Twitter mTwitter;
+
+    private static final String TWEETS_QUERY = "DevoxxFR";
+
+    public TwitterService() {
 		super(TAG);
 	}
 
 	@Override
 	public void onCreate() {
 		super.onCreate();
+        ConfigurationBuilder cb = new ConfigurationBuilder();
+        cb.setDebugEnabled(true)
+                .setOAuthConsumerKey("************")
+                .setOAuthConsumerSecret("****************")
+                .setOAuthAccessToken("***************")
+                .setOAuthAccessTokenSecret("**************");
+        TwitterFactory tf = new TwitterFactory(cb.build());
+        mTwitter = tf.getInstance();
+		mResolver = getContentResolver();
 
-		final HttpClient httpClient = RestService.getHttpClient(this);
-		final ContentResolver resolver = getContentResolver();
-		mExecutor = new RemoteJsonExecutor(httpClient, resolver);
-
-	}
+    }
 
 	@Override
 	protected void onHandleIntent(Intent intent) {
@@ -66,18 +84,54 @@ public class TwitterService extends IntentService {
 	public void triggerRefresh() {
 		try {
 			final SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
-			String refreshUrl = prefs.getString(JsonTwitterHandler.NEXT_REFRESH_URL, null);
-			if (refreshUrl == null) {
-				mExecutor.executeGet(DEFAULT_QUERY, new JsonTwitterHandler(getApplicationContext()));
-			} else {
-				mExecutor.executeGet(TWITTER_SEARCH_API_URL + refreshUrl, new JsonTwitterHandler(
-						getApplicationContext()));
+			long lastTweetId = prefs.getLong(LAST_TWEET_ID, 0);
+            Query query = new Query(TWEETS_QUERY);
+            if (lastTweetId > 0) {
+                query.setSinceId(lastTweetId);
 			}
-		} catch (HandlerException e) {
+            QueryResult result = mTwitter.search(query);
+            Log.i("Twitter","nombre de tweets recu:"+result.getTweets().size());
+            ArrayList<ContentProviderOperation> databaseOps = new ArrayList<ContentProviderOperation>();
+            long maxId = 0;
+            for (Status status : result.getTweets()) {
+                maxId = Math.max(maxId,status.getId());
+                databaseOps.add(buildCPOperation(status));
+            }
+            // batch update
+            if (! databaseOps.isEmpty()) {
+                mResolver.applyBatch(ScheduleContract.CONTENT_AUTHORITY, databaseOps);
+            }
+            if (maxId > 0) {
+                prefs.edit().putLong(LAST_TWEET_ID,maxId);
+            }
+
+        } catch (TwitterException e) {
 			// pas de message c'est qui doit pas avoir de reseau
 			// Toast.makeText(this, "Erreur durant la récupération des Tweets",
 			// Toast.LENGTH_SHORT).show();
-		}
-	}
+        } catch (RemoteException e) {
+            // pas de message
+            e.printStackTrace();
+        } catch (OperationApplicationException e) {
+            // pas de message
+            e.printStackTrace();
+        }
+    }
+
+    private ContentProviderOperation buildCPOperation(Status tweet) {
+
+        final ContentProviderOperation.Builder builder = ContentProviderOperation
+                .newInsert(ScheduleContract.Tweets.CONTENT_URI);
+
+        // affecte les valeurs des colonnes
+        builder.withValue(ScheduleContract.Tweets.TWEET_ID, tweet.getId());
+        builder.withValue(ScheduleContract.Tweets.CREATION_DATE, tweet.getCreatedAt().getTime());
+        builder.withValue(ScheduleContract.Tweets.IMAGE_URL, tweet.getUser().getProfileImageURL());
+        builder.withValue(ScheduleContract.Tweets.TEXT, tweet.getText());
+        builder.withValue(ScheduleContract.Tweets.USER, tweet.getUser().getName());
+        builder.withValue(ScheduleContract.Tweets.USER_NAME, tweet.getUser().getScreenName());
+
+        return builder.build();
+    }
 
 }
